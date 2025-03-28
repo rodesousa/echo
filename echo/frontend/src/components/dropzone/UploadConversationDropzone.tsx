a226d1539c3c492e568a65a0b844793cfea63bdd
Sameer Pashikanti <63326129+spashii@users.noreply.github.com>
2025-03-28 22:53:31 +0530

    block m4a uploads temporarily due to processing error (#89)
    block m4a uploads

--- echo/frontend/src/components/dropzone/UploadConversationDropzone.tsx
+++ echo/frontend/src/components/dropzone/UploadConversationDropzone.tsx
@@ -2,7 +2,6 @@ import { t } from "@lingui/core/macro";
 import { useProjectById, useUploadConversation } from "@/lib/query";
 import { LoadingOverlay } from "@mantine/core";
 import { PropsWithChildren } from "react";
-import { toast } from "../common/Toaster";
 import { CommonDropzone } from "./Dropzone";
 
 export const UploadConversationDropzone = (
@@ -14,6 +13,7 @@ export const UploadConversationDropzone = (
   }>,
 ) => {
   const uploadConversationMutation = useUploadConversation();
+
   const projectQuery = useProjectById({
     projectId: props.projectId,
   });
@@ -37,17 +37,19 @@ export const UploadConversationDropzone = (
       maxFiles={10}
       maxSize={200 * 1024 * 1024}
       onReject={(files) => {
-        toast.error(
+        alert(
           t`Something went wrong while uploading the file: ${files[0].errors[0].message}`,
         );
       }}
       loading={uploadConversationMutation.isPending}
       accept={[
-        "audio/m4a",
-        "audio/x-m4a",
         "audio/mp3",
         "audio/wav",
         "audio/mpeg",
+        "audio/ogg",
+        "audio/webm",
+        "video/mp4",
+        "audio/webm",
       ]}
     >
       {props.children}
